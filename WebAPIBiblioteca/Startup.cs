af7d4fd283b94ecf025dc1e89950a7957d953e55
Gerson Azabache <gerson_dunk@hotmail.com>
2021-07-20 16:37:25 -0500

    configurando contexto de datos y cadena de conexion

--- WebAPIBiblioteca/Startup.cs
+++ WebAPIBiblioteca/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +29,11 @@ namespace WebAPIBiblioteca
         {
 
             services.AddControllers();
+
+            //Configurando la conexión a la BD y el contexto de datos
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIBiblioteca", Version = "v1" });
