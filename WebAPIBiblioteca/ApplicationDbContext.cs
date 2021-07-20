657cd35d74f5ad43570f49cd7ad2804e18b4f7ce
Gerson Azabache <gerson_dunk@hotmail.com>
2021-07-20 16:16:18 -0500

    initial project

---
+++ WebAPIBiblioteca/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIBiblioteca.Entidades;
+
+namespace WebAPIBiblioteca
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Autor> Autor { get; set; } //el nombre de esta propiedad será el que tenga la tabla generada en la BD
+    }
+}
---
+++ WebAPIBiblioteca/Controllers/AutoresController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIBiblioteca.Entidades;
+
+namespace WebAPIBiblioteca.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            {
+                new Autor { Id = 1, Nombre = "Gerson" },
+                new Autor { Id = 2, Nombre = "Luis" },
+                new Autor { Id = 3, Nombre = "Antonio" }
+            };
+        }
+    }
+}
